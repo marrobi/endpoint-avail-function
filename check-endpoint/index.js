459f7844209e4859c6eb7ba8a17f6aa70aade119
Martin Peck <mpeck@microsoft.com>
2018-02-01 20:36:47 +0000

    fixed no url issue

--- check-endpoint/index.js
+++ check-endpoint/index.js
@@ -4,23 +4,27 @@ module.exports = function (context, req) {
 
     if (req.query.url) {
 
-        json_payload = {something: 'some_value'}
+        json_payload = {something: 'some_value'};
 
         var url = req.query.url;
         var data = { body: json_payload, json: true };
 
         request.post(url, data)
             .on("response", function(response){
-                msg = `Test complete. Status code: ${response.statusCode}`
-                console.log(msg)
+                msg = `Test complete. Status code: ${response.statusCode}`;
+                console.log(msg);
                 context.res = { status: response.statusCode, body: `Test complete. Status code: ${response.statusCode}`};
-                context.done() 
+                context.done();
             })
             .on("error", function(error){
-                msg = `Error performing test. Status code: ${response.statusCode}`
-                context.log(response)                
-                res = { status: 500, body: msg }; 
-                context.done()
+                msg = `Error performing test. Status code: ${response.statusCode}`;
+                context.log(response);
+                context.res  = { status: 500, body: msg };
+                context.done();
             })
-    } 
+    } else {
+        context.res  = { status: 400, body: "no url param supplied" }; 
+        context.done();
+    }
+    
 };
