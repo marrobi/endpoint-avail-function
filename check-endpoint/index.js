9a1140aa5fb8892d01f878fec286503da57b09b6
Martin Peck <mpeck@microsoft.com>
2018-02-01 20:27:55 +0000

    the start of something awesome

---
+++ check-endpoint/index.js
@@ -0,0 +1,26 @@
+var request = require('request');
+
+module.exports = function (context, req) {
+
+    if (req.query.url) {
+
+        json_payload = {something: 'some_value'}
+
+        var url = req.query.url;
+        var data = { body: json_payload, json: true };
+
+        request.post(url, data)
+            .on("response", function(response){
+                msg = `Test complete. Status code: ${response.statusCode}`
+                console.log(msg)
+                context.res = { status: response.statusCode, body: `Test complete. Status code: ${response.statusCode}`};
+                context.done() 
+            })
+            .on("error", function(error){
+                msg = `Error performing test. Status code: ${response.statusCode}`
+                context.log(response)                
+                res = { status: 500, body: msg }; 
+                context.done()
+            })
+    } 
+};
